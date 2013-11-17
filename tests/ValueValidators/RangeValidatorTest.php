207817dd33956cced4be6c77ef34a3efed9e5cd1
jeroendedauw <jeroendedauw@gmail.com>
2013-11-17 19:43:38 +0100

    Add to range validation test

--- tests/ValueValidators/RangeValidatorTest.php
+++ tests/ValueValidators/RangeValidatorTest.php
@@ -34,4 +34,34 @@ class RangeValidatorTest extends \PHPUnit_Framework_TestCase {
 		);
 	}
 
+	/**
+	 * @dataProvider withinBoundsProvider
+	 */
+	public function testNumberWithinRange_WhenSetWithIndividualSetters( $number, $lowerBound, $upperBound ) {
+		$rangeValidator = new RangeValidator();
+		$rangeValidator->setLowerBound( $lowerBound );
+		$rangeValidator->setUpperBound( $upperBound );
+
+		$this->assertTrue( $rangeValidator->validate( $number )->isValid() );
+	}
+
+	/**
+	 * @dataProvider outsideBoundsProvider
+	 */
+	public function testNumberOutsideRange_WhenSetWithSetRange( $number, $lowerBound, $upperBound ) {
+		$rangeValidator = new RangeValidator();
+		$rangeValidator->setRange( $lowerBound, $upperBound );
+
+		$this->assertFalse( $rangeValidator->validate( $number )->isValid() );
+	}
+
+	public function outsideBoundsProvider() {
+		return array(
+			array( 0, 1, 1 ),
+			array( -1, 0, 9 ),
+			array( 100, -200, 99 ),
+			array( -42, -41, 99 ),
+		);
+	}
+
 }
